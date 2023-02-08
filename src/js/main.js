26db06870c608288aad50e198aed1a3331979622
cyrilvernier <cyril@nanostud.io>
2023-02-08 20:07:07 +0100

    JS file structure

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+/* ----------------------------------
+  imports
+---------------------------------- */
+
+// imports here
+// register plugins here
+
+
+/* ----------------------------------
+  main code
+---------------------------------- */
+
+window.addEventListener( 'DOMContentLoaded', (e) => {
+
+  // your code here
+
+});
